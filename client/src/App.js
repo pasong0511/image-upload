1b3b96b058ac35ad1732d7ede2d4259107e22a15
park songhee <songhee6392@gmail.com>
2023-09-21 01:37:10 +0900

    리액트 라우터 돔의 link 추가해서 툴바 만들기

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { Routes, Route } from "react-router-dom";
+import Toolbar from "./components/Toolbar";
 
 const App = () => {
     const notify = () => toast("Wow so easy!");
@@ -13,7 +14,7 @@ const App = () => {
     return (
         <div style={{ maxWidth: 600, margin: "auto" }}>
             <ToastContainer />
-
+            <Toolbar />
             <Routes>
                 <Route path="/auth/register" exact element={<LoginPage />} />
                 <Route path="/auth/login" exact element={<RegisterPage />} />
---
+++ client/src/components/Toolbar.jsx
@@ -0,0 +1,22 @@
+//Toolbar.jsx
+import { Link } from "react-router-dom";
+
+const Toolbar = () => {
+    return (
+        <div>
+            <Link to="/">
+                <span>홈</span>
+            </Link>
+            <Link to="/auth/register">
+                <span style={{ float: "right" }}>로그인</span>
+            </Link>
+            <Link to="/auth/login">
+                <span style={{ float: "right", marginRight: 15 }}>
+                    회원가입
+                </span>
+            </Link>
+        </div>
+    );
+};
+
+export default Toolbar;
