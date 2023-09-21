a69240c12b0a3dca5be26e362483d64331c747eb
park songhee <songhee6392@gmail.com>
2023-09-21 22:33:16 +0900

    client : 회원가입 api 요청 구현

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import CustomInput from "../components/CustomInput";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const RegisterPage = () => {
     const [name, setName] = useState("");
@@ -23,6 +25,34 @@ const RegisterPage = () => {
         setPasswordCheck(e.target.value);
     };
 
+    const onSubmit = async (e) => {
+        try {
+            e.preventDefault();
+            if (username.length < 3)
+                throw new Error(
+                    "회원 id가 너무 짧아요 3글자 이상으로 해주세요!"
+                );
+            if (password.length < 6)
+                throw new Error(
+                    "비빌번호가 너무 짧아요 6글자 이상으로 해주세요!"
+                );
+            if (password !== passwordCheck) {
+                throw new Error("비밀번호가 다릅니다, 확인해주세요");
+            }
+            //console.log({ name, username, password });
+            const result = await axios.post("/users/register", {
+                name,
+                username,
+                password,
+            });
+
+            console.log(result);
+            toast.success("회원가입 성공~");
+        } catch (err) {
+            toast.error(err.message);
+        }
+    };
+
     return (
         <div
             style={{
@@ -33,7 +63,7 @@ const RegisterPage = () => {
             }}
         >
             <h3>회원가입</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <CustomInput
                     label={"이름"}
                     value={name}
@@ -56,6 +86,7 @@ const RegisterPage = () => {
                     onChange={onChangePasswordChek}
                     type="password"
                 />
+                <button type="submit">회원가입</button>
             </form>
         </div>
     );
